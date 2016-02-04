0b4c953ead40daa7cffd9aa26dfc4ba41451c9e2
Nic Smith <48gnomes@gmail.com>
2016-02-03 19:18:15 -0500

    Make a class that solves the Tower of Hanoi puzzle to the umpteenth number of disks.Displays each step in the console.

---
+++ cis237inclass2/Hanoi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass2
+{
+    class Hanoi
+    {
+
+        public void MoveDisk(int n, char source, char auxilary, char destination)
+        {          
+            //Base case to exit recursive loop.
+            if(n==1)
+            {
+                //Since we only have to move one disk.  (n = 1), we don't want to make another recursive call.  We just want to output the move that we are performing
+                Console.WriteLine("Move disk from tower {0} to tower {1}", source, destination);                
+            }
+            else
+            {
+                MoveDisk(n - 1, source, destination, auxilary);
+
+                MoveDisk(1, source, auxilary, destination);
+
+                MoveDisk(n - 1, auxilary, source, destination);
+            }
+        }
+    }
+}
--- cis237inclass2/Program.cs
+++ cis237inclass2/Program.cs
@@ -10,7 +10,35 @@ namespace cis237inclass2
     {
         static void Main(string[] args)
         {
-            Factorial factorial = new Factorial();
+            Console.WriteLine("Enter 1 for factorials or 2 for Tower of Hanoi.");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1" :
+                    Factorial factorial = new Factorial();
+                    break;
+
+                case "2" :
+                    Console.WriteLine("Enter number of disks on tower.  Try 5 or lower");
+                    userInput = Console.ReadLine();
+                    try
+                    {
+                        //DO Hanoi here
+                        Hanoi hanoi = new Hanoi();
+                        hanoi.MoveDisk(Convert.ToInt32(userInput), 'A', 'B', 'C');                        
+                    }
+                    catch
+                    {
+                        Console.WriteLine("You must Enter an int");
+                    }
+
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }            
         }
     }
 }
